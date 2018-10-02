8c95fd03b5db9f8b33ea970a11420011221d1e2c
sebahub <groovegeier@gmail.com>
2018-10-02 17:22:59 +0200

    start

---
+++ js/functions.js
@@ -0,0 +1,122 @@
+//- Variables
+
+let burger = document.querySelector('.nav-mobile-burger');
+
+let close = document.querySelector ('.nav-mobile-close');
+
+let navMobile = document.querySelector ('.nav-mobile');
+
+let buns = document.querySelectorAll ('.bun');
+
+//- Burger-Menu
+
+close.addEventListener('click', (event) => {
+    navMobile.classList.remove('visible');
+
+});
+
+burger.addEventListener('click', (event) => {
+  navMobile.classList.toggle('visible');
+  //buns.classList.toggle('animation');
+  
+  buns.forEach((element) => {
+    element.classList.toggle('animation');
+
+    });
+
+});
+
+const TypeWriter = function(txtElement, words, wait = 3000) {
+    this.txtElement = txtElement,
+    this.words = words;
+    this.txt = '';
+    this.wordIndex = 0;
+    this.wait = parseInt(wait, 10);
+    this.type();
+    this.isDeleting = false;
+
+}
+
+// Type Method
+
+TypeWriter.prototype.type = function() {
+    // Current index of Word
+    const current = this.wordIndex % this.words.length;
+    // Get full text of current Word
+    const fullTxt = this.words[current];
+
+    // Check if deleting
+    if(this.isDeleting) {
+        //Remove char
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        // Add char
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    // Insert txt into element
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+    // Type Speed
+    let typeSpeed = 300;
+
+    if(this.isDeleting) {
+        typeSpeed /= 2;
+
+    }
+
+    // If word is complete
+    if(!this.isDeleting && this.txt == fullTxt) {
+        //Make pause at end
+        typeSpeed = this.wait;
+        // Set delete to true
+        this.isDeleting = true;
+    } else if(this.isDeleting && this.txt == '') {
+        this.isDeleting = false;
+        // Move to next Word
+        this.wordIndex++;
+        // Pause before start typing
+        typeSpeed = 500;
+    }
+
+    setTimeout(() => this.type(), typeSpeed)
+}
+
+// Init On Dom Load
+document.addEventListener('DOMContentLoaded', init);
+
+// Init App
+function init() {
+    const txtElement = document.querySelector('.txt-type');
+    const words = JSON.parse(txtElement.getAttribute('data-words'));
+    const wait = txtElement.getAttribute('data-wait');
+
+    // Innit Typewriter
+    new TypeWriter(txtElement, words, wait);
+
+}
+
+// OnScroll Event
+
+
+let printAnimation = document.querySelector('.print');
+
+
+window.addEventListener('scroll', () => {
+    // Maximum Scroll
+    const scrollable = document.documentElement.scrollHeight - window.innerHeight;
+
+    // OnScroll-Animation
+
+
+    const scrolled = window.scrollY;
+
+    if (scrolled >= 150) {
+        //printAnimation.classList.add('print-animation');
+
+    }
+
+    console.log(scrolled);
+    
+
+});
